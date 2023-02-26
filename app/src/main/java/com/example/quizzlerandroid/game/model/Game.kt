2a1de805427bfcf93fa80976feb634637a6c6248
Sbusiso Mabena <Sbusisomabena0@gmail.com>
2023-02-27 00:10:30 +0200

    feat: add nextQuestion functionality in Game

--- app/src/main/java/com/example/quizzlerandroid/game/model/Game.kt
+++ app/src/main/java/com/example/quizzlerandroid/game/model/Game.kt
@@ -1,11 +1,22 @@
 package com.example.quizzlerandroid.game.model
 
-class Game {
+class Game(private val questions: List<Question>) {
     var currentScore: Int = 0
         private set
 
+    private var currentQuestionIndex = -1
+
     fun incrementScore() {
         currentScore++
+    }
+
+    fun nextQuestion(): Question? {
+        return if (currentQuestionIndex + 1 < questions.size) {
+            currentQuestionIndex++
+            questions[currentQuestionIndex]
+        } else {
+            null
+        }
     }
 
 }
--- app/src/test/java/com/example/quizzlerandroid/game/model/GameUnitTests.kt
+++ app/src/test/java/com/example/quizzlerandroid/game/model/GameUnitTests.kt
@@ -1,15 +1,44 @@
 package com.example.quizzlerandroid.game.model
 
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
 import org.junit.Test
 
 class GameUnitTests {
+    private lateinit var game: Game
+
+    @Before
+    fun setup() {
+        game = Game(listOf())
+    }
+
     @Test
     fun `when incrementing the score, it should increment the current score`() {
-        val game = Game()
-
         game.incrementScore()
 
-        Assert.assertEquals(1, game.currentScore)
+        assertEquals(1, game.currentScore)
+    }
+
+    @Test
+    fun `when getting the next question, it should return the next question in the list`() {
+        val question = Question("::question::", true)
+        val game = Game(listOf(question))
+
+        val nextQuestion = game.nextQuestion()
+
+        assertEquals(question, nextQuestion)
+    }
+
+    @Test
+    fun `when getting the next question and the end of the list has been reached, it should return null`() {
+
+        val question = Question("::question::", true)
+        val game = Game(listOf(question))
+
+        game.nextQuestion()
+        val nextQuestion = game.nextQuestion()
+
+        assertNull(nextQuestion)
     }
 }
